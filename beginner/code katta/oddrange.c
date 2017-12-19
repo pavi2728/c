06480d9046c419a4025556846adc670582eb96f2
pavi2728 <34670250+pavi2728@users.noreply.github.com>
2017-12-19 15:24:29 +0530

    Create oddrange.c

---
+++ beginner/code katta/oddrange.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int n,m,i;
+    scanf("%d,%d",&n,&m);
+    for(i=n;i<=m;i++)
+    {
+    if(i%2!=0)
+    {
+     printf("%d\n",i);
+    }
+    }
+
+	return 0;
+}
