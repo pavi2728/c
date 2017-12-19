de1aba5d9858fac0882d501edca1fe25d0a7e62d
pavi2728 <34670250+pavi2728@users.noreply.github.com>
2017-12-19 12:37:39 +0530

    Create alphabet.c

---
+++ beginner/code katta/alphabet.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void)
+{
+	char  c;
+  int n;
+	scanf("%c",&c);
+  n=((int) c);
+	if(n>=65 && n<=122)
+	printf("alphabet");
+	else
+	printf("not alphabet");
+	return 0;
+}
