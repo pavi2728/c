4099c452dceaa919d0dc11c96e3523c472817602
pavi2728 <34670250+pavi2728@users.noreply.github.com>
2017-12-21 20:36:35 +0530

    Update removingvowel.c

--- beg/assign/removingvowel.c
+++ beg/assign/removingvowel.c
@@ -1,18 +1,13 @@
-include<stdio.h>
 #include<string.h>
 int main(void)
 {
 	char c[10],rev[10];
-	int i,length,j;
+	int i,length,j,l;
 	scanf("%s",c);
 	length=strlen(c);
 	for(i=0,j=length;i<length&&j>0;i++,j--)
 	{
-	
-	
-		
-		rev[j]=c[i];
-	
+	rev[j]=c[i];
 	}
 	for(j=0;j<=length;j++)
 	printf("%c",rev[j]);
@@ -20,13 +15,18 @@ int main(void)
 	{
 		if(rev[i]=='a'||rev[i]=='e'||rev[i]=='i'||rev[i]=='o'||rev[i]=='u')
 		{
-		rev[i]=rev[i+1];
+		for(j=i;j<length;j++)
+		{
+		rev[j]=rev[j+1];
 		}
-		
+		length--;
+		}
 	}
-	int l=strlen(rev);
-	for(i=0;i<l;i++)
+
+	for(i=0;i<=length;i++)
 	printf("\n%c",rev[i]);
 	return 0;
 	
 }
+
+
