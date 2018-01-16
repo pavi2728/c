21526d84cadfb7c332d1b3126effb2796f11c2cd
pavi2728 <34670250+pavi2728@users.noreply.github.com>
2018-01-16 21:36:54 +0530

    Create count_words.java

---
+++ beginner/code katta/set 4/count_words.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+        String a=s.nextLine();
+        String arr[]=a.split("\\s+");
+        int l=arr.length;
+        System.out.println(l);
+	}
+}
