03f04de6cc123d1d8234041af737c186c1e35b35
pavi2728 <34670250+pavi2728@users.noreply.github.com>
2018-01-16 21:50:23 +0530

    Create count_no_of_spaces.java

---
+++ beginner/code katta/set 4/count_no_of_spaces.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+       
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        char c[]=a.toCharArray();
+        int count=0;
+        for(int i=0;i<a.length();i++)
+       {
+          if(c[i]!=' ')
+          count++;
+
+	}
+  int t=a.length()-count;
+   System.out.println(t);
+}
+}
