c9de3f4e6af58ed8234aec08d1410101e4ff5872
pavi2728 <34670250+pavi2728@users.noreply.github.com>
2018-02-01 19:38:33 +0530

    Create print_char.c

---
+++ beginner/code katta/set 7/print_char.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+	char s[100];
+    int i,k;
+    scanf("%s %d",s,&k);
+     for(i=0;i<k;i++)
+    { 
+       printf("%c",s[i]);
+     }
+
+   
+return 0;
+}
