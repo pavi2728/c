26dd7cd09dee176384d74b64477e7189590b141f
pavi2728 <34670250+pavi2728@users.noreply.github.com>
2018-03-04 20:57:08 +0530

    Create string_palindrome.c

---
+++ beginner/code katta/set 8/string_palindrome.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+char a[100],b[100];
+int l=0,i,flag=0;
+scanf("%s",a);
+l=strlen(a);
+for(i=l-1;i>=0;i--)
+{
+b[l-i-1]=a[i];
+}
+for(flag=1,i=0;i<l;i++)
+{
+if(b[i]!=a[i])
+flag=0;
+}
+if(flag==1)
+printf("yes");
+else
+printf("no");
+return 0;
+}
