908bf734e620be96112734a92bc982b73e734fd0
pavi2728 <34670250+pavi2728@users.noreply.github.com>
2018-03-08 21:38:19 +0530

    Update difference.c

--- beginner/code katta/set 9/difference.c
+++ beginner/code katta/set 9/difference.c
@@ -2,8 +2,12 @@
 #include<math.h>
 int main(void) {
 	long int a,b,diff=0;
+	 int i;
+	for(i=0;i<3;i++)
+	{
     scanf("%ld %ld",&a,&b);
     diff=abs(a-b);
     printf("%ld",diff);
+	}
 	return 0;
 }
