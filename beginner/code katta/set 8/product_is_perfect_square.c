a1ec9a2e12935579d10fb9946328319e843f6839
pavi2728 <34670250+pavi2728@users.noreply.github.com>
2018-03-08 20:14:10 +0530

    Update product_is_perfect_square.c

--- beginner/code katta/set 8/product_is_perfect_square.c
+++ beginner/code katta/set 8/product_is_perfect_square.c
@@ -1,12 +1,20 @@
+#include <stdio.h>
+
 int main(void) {
-	int a,b,n,l,m;
+	int a,b,n,i;
     scanf("%d %d",&a,&b);
     n=a*b;
-    l=n/a;
-    m=n/b;
-    if(l==m)
-    printf("yes");
-    else
+    for(i=0;i<=n;i++)
+    {
+      if(n==i*i)
+{
+      printf("yes");
+      return 0;
+}
+      
+}
     printf("no");
     return 0;
 }
+
+
