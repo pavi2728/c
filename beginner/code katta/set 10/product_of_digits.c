9596911c56b1f8c5b8fe2f7473c5dfc074d7d4b6
pavi2728 <34670250+pavi2728@users.noreply.github.com>
2018-03-11 11:40:14 +0530

    Create product_of_digits.c

---
+++ beginner/code katta/set 10/product_of_digits.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void) {
+	         int n,prod=1,dig;
+             scanf("%d",&n);
+             while(n!=0)
+             {
+                 dig=n%10;
+                  prod=prod*dig;
+                 n=n/10;
+             }
+              printf("%d",prod);
+              return 0;
+}
