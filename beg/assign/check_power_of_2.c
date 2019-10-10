31ba9116c1cd8748565f5382dc478ffc109ca791
NishanthStark <56387570+NishanthStark@users.noreply.github.com>
2019-10-10 14:29:23 +0530

    corrected minor errors and variables

--- beg/assign/check_power_of_2.c
+++ beg/assign/check_power_of_2.c
@@ -1,20 +1,18 @@
 #include<stdio.h>
-
-int main(void)
+main()
 {
-          int n,i,status=0;
+          int n,i,s=0;
           scanf("%d",&n);
          while(n!=1)
           {
           	if(n%2!=0){
-          	status=1;
+          	s=1;
           	break;
           	}
           	n=n/2;
           }
-          if(status==1)
-          printf("not power of 2");
+          if(s==1)
+          printf("not a power of 2");
           else
           printf("power of 2");
-          return 0;
-          }
+}
